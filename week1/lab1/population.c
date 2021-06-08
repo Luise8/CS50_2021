62954a28f30238e74c7a330a352aef1f5ca8d4cf
Luis E. Gamez P <82817895+Luise8@users.noreply.github.com>
2021-06-08 15:01:07 -0400

    Update population.c

--- week1/lab1/population.c
+++ week1/lab1/population.c
@@ -1,14 +1,31 @@
-nclude <cs50.h>
+#include <cs50.h>
 #include <stdio.h>
-
 int main(void)
 {
-    for (int i = 0; i < 3; i++)
+    // TODO: Prompt for start size
+    int startsize;
+    do
     {
-        for (int j = 0; j < 3; j++)
-        {
-            printf("#");
-        }
-        printf("\n");
+        startsize = get_int("Start population?\n");
     }
+    while (startsize < 9);
+
+    // TODO: Prompt for end size
+    int endsize;
+    do
+    {
+        endsize = get_int("End population?\n");
+    }
+    while (endsize < startsize);
+    // TODO: Calculate number of years until we reach threshold
+
+    int n = startsize;
+    int years = 0;
+    while (n < endsize)
+    {
+        n = n + (n / 3) - (n / 4);
+        years++;
+    }
+    // TODO: Print number of years
+    printf("Years: %i", years);
 }
